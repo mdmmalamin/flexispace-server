073235d0ad24f9b7c695e21e1e2a41a43f8dcfc8
mdmmalamin <mdmmalamin@gmail.com>
2024-09-09 20:07:42 +0600

    add slotSchema model

--- src/app/modules/slot/slot.model.ts
+++ src/app/modules/slot/slot.model.ts
@@ -4,8 +4,26 @@ import { TSlot } from './slot.interface';
 const slotSchema = new Schema<TSlot>(
   {
     room: {
-      type: Schema.Types.ObjectId
-    }
+      type: Schema.Types.ObjectId,
+      ref: 'Room',
+      required: true,
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+    startTime: {
+      type: String,
+      required: true,
+    },
+    endTime: {
+      type: String,
+      required: true,
+    },
+    isBooked: {
+      type: Boolean,
+      default: false,
+    },
   },
   {
     timestamps: true,
