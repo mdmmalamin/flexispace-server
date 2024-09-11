5c75ec0bfa5c23448616dd4910530b97d46283be
Md Moinur Mahbub Al Amin <mdmmalamin@gmail.com>
2024-09-11 16:00:00 +0600

    Update slot.utils.ts | formateTime

--- src/app/modules/slot/slot.utils.ts
+++ src/app/modules/slot/slot.utils.ts
@@ -1,4 +1,7 @@
-export const formatTime = (hours: number, minutes: number): string => {
+export const formatTime = (totalMinutes: number): string => {
+		const hours = totalMinutes / 60;
+		const minutes = totalMinutes % 60;
+
   const formattedHours = String(hours).padStart(2, '0');
   const formattedMinutes = String(minutes).padStart(2, '0');
   return `${formattedHours}:${formattedMinutes}`;
