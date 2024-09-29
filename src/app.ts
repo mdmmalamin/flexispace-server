e105d468ae2f34549c78cbf344a692abe3bc0ee2
mdmmalamin <mdmmalamin@gmail.com>
2024-09-30 05:20:18 +0600

    allow credentials

--- src/app.ts
+++ src/app.ts
@@ -14,6 +14,7 @@ app.use(cookieParser());
 app.use(
   cors({
     origin: ['http://localhost:5173', 'https://flexispace.netlify.app/'],
+    credentials: true, // Allow credentials (cookies, authorization headers)
   }),
 );
 
